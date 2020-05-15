917c9af53c0aeede5a10eff0163b244c8db2b331
George-Pereira <George.fpereira06@gmail.com>
2020-05-15 17:45:51 -0300

    Criação de Daos e entidades

--- src/entity/Disciplina.java
+++ src/entity/Disciplina.java
@@ -1,5 +1,20 @@
 package entity;
 
-public class Disciplina {
-
+public class Disciplina 
+{
+	private String nome;
+	private String sigla;
+	private String turno;
+	private int Naulas;
+	
+	public Disciplina(String nome, String sigla, String turno, int Naulas) 
+	{
+		this.nome = nome;
+		this.sigla = sigla;
+		this.turno = turno;
+		this.Naulas = Naulas;
+	}
+	public Disciplina() 
+	{
+	}
 }
